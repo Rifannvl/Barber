d2a94103f7a15d9c2a7444ed2fe745339e05f3d1
Rifannvl <riffanafendi@gmail.com>
2025-08-04 19:19:54 +0700

    update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,9 @@
+import React from "react";
+import BookingPage from "../components/BookingPage";
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Home</h1>;
+  return (
+    <div>
+      <BookingPage />
+    </div>
+  );
 }
