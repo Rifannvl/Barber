47702a04e3259e1f56665a91427b441ff2ab45e0
Rifannvl <riffanafendi@gmail.com>
2025-08-04 21:44:19 +0700

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "dark-bg": "#1A1A1A",
-        "light-text": "#F0F0F0",
-        "brand-blue": "#00A9FF",
-        "dark-card": "#2C2C2C",
+        "dark-bg": "#111827",
+        "dark-card": "#1F2937",
+        "brand-gold": "#B9945A", // <-- Warna Emas
+        "light-text": "#F3F4F6",
+        // Warna untuk Light Mode
+        "light-bg": "#F9FAFB", // Abu-abu sangat terang
+        "light-card": "#FFFFFF", // Putih
+        "dark-text": "#111827", // Kebalikan dari light-text
       },
       fontFamily: {
-        sans: ["Poppins", "sans-serif"],
-        display: ["Syne", "sans-serif"],
+        sans: ["Inter", "sans-serif"], // <-- Font untuk teks biasa
+        display: ["Playfair Display", "serif"], // <-- Font untuk judul
       },
     },
   },
