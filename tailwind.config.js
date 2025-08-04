d1b103ef74b8197e88a91186bd480d6c3e80a4dc
Rifannvl <riffanafendi@gmail.com>
2025-08-04 19:18:19 +0700

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-bg": "#1A1A1A",
+        "light-text": "#F0F0F0",
+        "brand-blue": "#00A9FF",
+        "dark-card": "#2C2C2C",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+        display: ["Syne", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
